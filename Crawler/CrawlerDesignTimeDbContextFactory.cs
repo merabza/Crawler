8c32cf4b76144336ff505a29c796831e11b21a11
merabza <merabza@gmail.com>
2024-03-20 13:49:22 +0400

    202403201349

--- Crawler/CrawlerDesignTimeDbContextFactory.cs
+++ Crawler/CrawlerDesignTimeDbContextFactory.cs
@@ -4,10 +4,12 @@ namespace Crawler;
 
 //ეს კლასი საჭიროა იმისათვის, რომ შესაძლებელი გახდეს მიგრაციასთან მუშაობა.
 //ანუ დეველოპერ ბაზის წაშლა და ახლიდან დაგენერირება, ან მიგრაციაში ცვლილებების გაკეთება
+// ReSharper disable once UnusedType.Global
 public sealed class CrawlerDesignTimeDbContextFactory : DesignTimeDbContextFactory<CrawlerDbContext>
 {
+    // ReSharper disable once ConvertToPrimaryConstructor
     public CrawlerDesignTimeDbContextFactory() : base("CrawlerDbMigration", "ConnectionString",
-        "D:\\1WorkSecurity\\Crawler\\CrawlerDb.json")
+        @"D:\1WorkSecurity\Crawler\CrawlerDb.json")
     {
     }
 }
