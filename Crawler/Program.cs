afa4ebf367300571979c87f1151d150dbbc5af2b
merabza <merabza@gmail.com>
2026-08-22 18:50:45 +0400

    202608221850

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -23,7 +23,7 @@ try
             break;
         case EParseResult.Usage:
             return 1;
-        case EParseResult.Error:
+        case EParseResult.ParseError:
             return 2;
         default:
             throw new SwitchExpressionException();
